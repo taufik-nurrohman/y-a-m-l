1bd61f915ee48a4d22d07d7b0680e4bfeed21f54
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-09-24 12:13:41 +0700

    Update

--- from.php
+++ from.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace x\y_a_m_l {
-    function from(?string $value, $array = false) {
+    function from(?string $value, $array = false, array &$lot = []) {
         if ("" === ($value = \trim($raw = $value ?? ""))) {
             return null;
         }
@@ -32,17 +32,60 @@ namespace x\y_a_m_l {
         if ('#' === $value[0]) {
             return null;
         }
+        // <https://yaml.org/spec/1.2.2#692-node-anchors>
+        if (false !== \strpos('&*', $value[0]) && \preg_match('/^([&*])([^\s,\[\]{}]+)(\s+|$)/', $value, $m)) {
+            if ('&' === $m[1]) {
+                $lot[0][$m[2]] = from($value = \substr($value, \strlen($m[0])), $array, $lot);
+                $value = &$lot[0][$m[2]];
+                return $value;
+            }
+            return $lot[0][$m[2]] ?? null;
+        }
         // List-style value
-        if (0 === \strpos($value, '- ')) {
+        if ('-' === $value[0] && \strlen($value) > 2 && false !== \strpos(" \n\t", $value[1])) {
             $out = [];
-            foreach (\explode("\n- ", \substr($value, 2) as $v) {
-                $out[] = from($v, $array);
+            foreach (\preg_split('/\n-[ \n\t]/', \substr($value, 2)) as $v) {
+                $out[] = from($v, $array, $lot);
             }
             return $out;
         }
         // Fold-style or literal-style value
         if (false !== \strpos('>|', $value[0])) {
-            [$a, $b] = \explode("\n", $value, 2);
+            [$a, $b] = \explode("\n", $raw, 2);
+            $a = \trim(\strstr($a, '#', true) ?: $a);
+            $b = \preg_replace('/^#.*$/m', "", $b);
+            $dent = \strspn(\trim($b, "\n"), ' ');
+            $b = \substr("\n" . \strtr($b, [
+                "\n" . \str_repeat(' ', $dent) => "\n"
+            ]), 1);
+            if (isset($a[1])) {
+                $chomp = \substr($a, -1);
+                if (\is_numeric($chomp)) {
+                    $chomp = "";
+                    $dent = (int) \substr($a, 1);
+                } else {
+                    $dent = (int) \substr($a, 1, -1);
+                }
+                if ($dent > 0) {
+                    $b = \substr(\strtr("\n" . ($d = \str_repeat(' ', $dent)) . \strtr($b, [
+                        "\n" => "\n" . $d
+                    ]), [
+                        "\n" . $d . "\n" => "\n\n"
+                    ]), 1);
+                }
+            } else {
+                $chomp = "";
+            }
+            if ("" !== $chomp && false === \strpos('+-', $chomp)) {
+                return null; // :(
+            }
+            if ('+' !== $chomp) {
+                $b = \rtrim($b) . ("" === $chomp ? "\n" : "");
+            }
+            if ('>' === $a[0]) {
+                // TODO
+            }
+            return $b;
         }
         if (\is_numeric($value)) {
             if (\strlen($value) > 1 && '0' === $value[0] && false === \strpos($value, '.')) {
@@ -63,12 +106,18 @@ namespace x\y_a_m_l {
                     $out .= \strtolower($v);
                     continue;
                 }
-                $out .= false !== \strpos(',:[]{}', $v) ? $v : \json_encode($v, false, 1);
+                if (\is_numeric($v)) {
+                    $out .= $v;
+                    continue;
+                }
+                $out .= false !== \strpos(',:[]{}', $v) ? $v : \json_encode(from($v, $array, $lot), false, 1);
             }
             return \json_decode($out) ?? $value;
         }
         if ("'" === $value[0] && "'" === \substr($value, -1)) {
-            return \substr($value, 1, -1);
+            return \strtr(\substr($value, 1, -1), [
+                "''" => "'"
+            ]);
         }
         if ('"' === $value[0] && '"' === \substr($value, -1)) {
             try {
@@ -80,7 +129,10 @@ namespace x\y_a_m_l {
             }
             return $value;
         }
-        $block = 0;
+        if (false === ($n = \strpos($value, ':')) || false === \strpos(" \t", \substr($value, $n + 1, 1))) {
+            return \trim(\preg_replace('/\s+/', ' ', \strstr($value, '#', true) ?: $value));
+        }
+        $block = -1;
         $blocks = [];
         $rows = \explode("\n", $value);
         foreach ($rows as $row) {
@@ -97,14 +149,14 @@ namespace x\y_a_m_l {
                     continue;
                 }
                 // A list
-                //if ('-' === \trim(\strstr($current, '#', true))) {
-                //    $blocks[$block] .= "\n- ";
-                //    continue;
-                //}
-                //if (0 === \strpos($current, '- ')) {
-                //    $blocks[$block] .= "\n" . $current;
-                //    continue;
-                //}
+                if ('-' === \trim(\strstr($current, '#', true) ?: $current)) {
+                    $blocks[$block] .= "\n- ";
+                    continue;
+                }
+                if ('-' === $current[0] && false !== \strpos(" \t", $current[1])) {
+                    $blocks[$block] .= "\n- " . \substr($current, 2);
+                    continue;
+                }
                 if ($dent > 0) {
                     if ("\n- " === \substr($prev, -3)) {
                         $blocks[$block] .= $current;
@@ -116,6 +168,16 @@ namespace x\y_a_m_l {
             }
             $blocks[++$block] = $current;
         }
+        $out = [];
+        foreach ($blocks as $block) {
+            if (false !== \strpos('\'"', $block[0]) && \preg_match('/^(' . $str . '):\s+/', $block, $m)) {
+                $out[from($m[1])] = from(\substr($block, \strlen($m[0])), $array, $lot);
+                continue;
+            }
+            $any = \preg_split('/:\s+/', $block, 2);
+            $out[$any[0]] = isset($any[1]) ? from($any[1], $array, $lot) : null;
+        }
+        return $out;
     }
 }
 
---
+++ test/from.php
@@ -0,0 +1,105 @@
+<?php
+
+if (!in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
+    exit;
+}
+
+error_reporting(E_ALL | E_STRICT);
+
+ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
+ini_set('html_errors', 1);
+
+define('D', DIRECTORY_SEPARATOR);
+define('P', "\u{001A}");
+define('PATH', __DIR__);
+
+require __DIR__ . D . '..' . D . 'from.php';
+
+$test = basename($_GET['test'] ?? 'scalar');
+$view = $_GET['view'] ?? 'php';
+
+$files = glob(__DIR__ . D . 'from' . D . $test . D . '*.yaml', GLOB_NOSORT);
+usort($files, static function ($a, $b) {
+    $a = dirname($a) . D . basename($a, '.yaml');
+    $b = dirname($b) . D . basename($b, '.yaml');
+    return strnatcmp($a, $b);
+});
+
+$out = '<!DOCTYPE html>';
+$out .= '<html dir="ltr">';
+$out .= '<head>';
+$out .= '<meta charset="utf-8">';
+$out .= '<title>';
+$out .= 'YAML to Data';
+$out .= '</title>';
+$out .= '</head>';
+$out .= '<body>';
+
+$out .= '<form method="get">';
+
+$out .= '<fieldset>';
+$out .= '<legend>';
+$out .= 'Navigation';
+$out .= '</legend>';
+$out .= '<a href="to.php">Data to YAML</a>';
+$out .= '</fieldset>';
+
+$out .= '<fieldset>';
+$out .= '<legend>';
+$out .= 'Filter';
+$out .= '</legend>';
+$out .= '<button' . ('*' === $test ? ' disabled' : "") . ' name="test" type="submit" value="*">';
+$out .= '*';
+$out .= '</button>';
+foreach (glob(__DIR__ . D . 'from' . D . '*', GLOB_ONLYDIR) as $v) {
+    $out .= ' ';
+    $out .= '<button' . ($test === ($n = basename($v)) ? ' disabled' : "") . ' name="test" type="submit" value="' . htmlspecialchars($n) . '">';
+    $out .= htmlspecialchars($n);
+    $out .= '</button>';
+}
+$out .= ' ';
+$out .= '<button' . ('TEST' === $test ? ' disabled' : "") . ' name="test" type="submit" value="TEST">';
+$out .= 'TEST';
+$out .= '</button>';
+$out .= '</fieldset>';
+
+$out .= '<fieldset>';
+$out .= '<legend>';
+$out .= 'Preview';
+$out .= '</legend>';
+$out .= '<select name="view">';
+$out .= '<option' . ('json' === $view ? ' selected' : "") . ' value="json">JSON</option>';
+$out .= '<option' . ('php' === $view ? ' selected' : "") . ' value="php">PHP</option>';
+$out .= '</select>';
+$out .= ' ';
+$out .= '<button name="test" type="submit" value="' . $test . '">';
+$out .= 'Update';
+$out .= '</button>';
+$out .= '</fieldset>';
+
+$out .= '</form>';
+
+foreach ($files as $v) {
+    $content = "";
+    $raw = file_get_contents($v);
+    $out .= '<h1 id="' . ($n = basename(dirname($v)) . ':' . basename($v, '.yaml')) . '"><a aria-hidden="true" href="#' . $n . '">&sect;</a> ' . strtr($v, [PATH . D => '.' . D]) . '</h1>';
+    $out .= '<div style="display:flex;gap:1em;margin:1em 0 0;">';
+    $out .= '<pre style="background:#ccc;border:1px solid rgba(0,0,0,.25);color:#000;flex:1;font:normal normal 100%/1.25 monospace;margin:0;padding:.5em;tab-size:4;white-space:pre-wrap;word-wrap:break-word;">';
+    $out .= htmlspecialchars($raw);
+    $out .= '</pre>';
+    $start = microtime(true);
+    $out .= '<pre style="background:#cfc;border:1px solid rgba(0,0,0,.25);color:#000;flex:1;font:normal normal 100%/1.25 monospace;margin:0;padding:.5em;tab-size:4;white-space:pre-wrap;word-wrap:break-word;">';
+    $data = x\y_a_m_l\from($raw);
+    $out .= htmlspecialchars('php' === $view ? var_export($data, true) : json_encode($data));
+    $out .= '</pre>';
+    $end = microtime(true);
+    $out .= '</div>';
+    $time = round(($end - $start) * 1000, 2);
+    $out .= '<p style="color:#' . ($time >= 1 ? '800' : '080') . ';">Parsed in ' . $time . ' ms.</p>';
+}
+
+$out .= '</body>';
+$out .= '</html>';
+
+echo $out;
