e58a6535fb1c371d654854a6b4943d7f750b48bf
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-10-01 09:35:28 +0700

    Update

--- from.php
+++ from.php
@@ -2,208 +2,7 @@
 
 namespace x\y_a_m_l {
     function from(?string $value, $array = false, array &$lot = []) {
-        if ("" === ($value = \trim($raw = $value ?? ""))) {
-            return null;
-        }
-        // Normalize line break(s)
-        $value = \strtr($value, [
-            "\r\n" => "\n",
-            "\r" => "\n"
-        ]);
-        if ("" === ($value = \trim(from\c($value)))) {
-            return null;
-        }
-        if (\array_key_exists($var = \strtolower($value), $vars = [
-            "''" => "",
-            '""' => "",
-            '+.inf' => \INF,
-            '+.nan' => \NAN,
-            '-.inf' => -\INF,
-            '-.nan' => -\NAN,
-            '.inf' => \INF,
-            '.nan' => \NAN,
-            '[]' => [],
-            'false' => false,
-            'null' => null,
-            'true' => true,
-            '{}' => $array ? [] : (object) [],
-            '~' => null
-        ])) {
-            return $vars[$var];
-        }
-        if ('"' === $value[0] && '"' === \substr($value, -1)) {
-            return \stripcslashes(from\f(\strtr(\substr($value, 1, -1), [
-                "\\\n" => ""
-            ])));
-        }
-        if ("'" === $value[0] && "'" === \substr($value, -1)) {
-            return from\f(\strtr(\substr($value, 1, -1), [
-                "''" => "'"
-            ]), false);
-        }
-        // Fold-style or literal-style value
-        if (false !== \strpos('>|', $value[0])) {
-            [$rule, $content] = \array_replace(["", ""], \explode("\n", from\c(\ltrim($raw)), 2));
-            $dent = \strspn(\trim($content, "\n"), ' ');
-            $content = \substr(\strtr("\n" . $content, [
-                "\n" . \str_repeat(' ', $dent) => "\n"
-            ]), 1);
-            if (isset($rule[1])) {
-                $cut = \substr($rule, -1);
-                // `>4`
-                if (\is_numeric($cut)) {
-                    $cut = "";
-                    $dent = (int) \substr($rule, 1);
-                // `>4+`
-                } else {
-                    $dent = (int) \substr($rule, 1, -1);
-                }
-            // `>`
-            } else {
-                $cut = "";
-                $dent = 0;
-            }
-            if ("" !== $cut && false === \strpos('+-', $cut)) {
-                return null; // :(
-            }
-            if ('+' !== $cut) {
-                $content = \rtrim($content) . ("" === $cut ? "\n" : "");
-            }
-            if ('>' === $rule[0]) {
-                $content = from\f($content);
-            }
-            if ($dent > 0) {
-                $d = \str_repeat(' ', $dent);
-                $content = \substr(\strtr(\strtr("\n" . $content, [
-                    "\n" => "\n" . $d
-                ]), [
-                    "\n" . $d . "\n" => "\n\n"
-                ]), 1);
-            }
-            return $content;
-        }
-        if ('[' === $value[0] && ']' === \substr($value, -1) || '{' === $value[0] && '}' === \substr($value, -1)) {
-            return from(from\r($value), $array, $lot);
-        }
-        // A tag
-        if ('!' === $value[0]) {
-            [$tag, $content] = \array_replace(["", ""], \preg_split('/\s+/', $value, 2, \PREG_SPLIT_NO_EMPTY));
-            $value = from($content, $array, $lot);
-            if ('!!str' === $tag && !isset($lot[$tag]) && $value instanceof \DateTime) {
-                return $content;
-            }
-            return from\t($value, $array, $lot, $tag);
-        }
-        // <https://yaml.org/spec/1.2.2#692-node-anchors>
-        if (false !== \strpos('&*', $value[0]) && \preg_match('/^([&*])([^\s,\[\]{}]+)(\s+|$)/', $value, $m)) {
-            $key = '&' . $m[2];
-            if ('&' === $m[1]) {
-                $value = from(\substr($value, \strlen($m[0])), $array, $lot);
-                if (!isset($lot[0][$key])) {
-                    $lot[0][$key] = &$value;
-                }
-                return $value;
-            }
-            return $lot[0][$key] ?? null;
-        }
-        // List-style value
-        if ('-' === $value[0] && \strlen($value) > 2 && false !== \strpos(" \n\t", $value[1])) {
-            $out = [];
-            foreach (\preg_split('/\n-[ \n\t]/', \substr($value, 2)) as $v) {
-                if (0 === \strpos($v, '- ')) {
-                    $v = \strtr($v, [
-                        "\n  " => "\n"
-                    ]);
-                } else {
-                    $v = \trim($v);
-                }
-                $out[] = from($v, $array, $lot);
-            }
-            return $out;
-        }
-        if (\strlen($value) > 2 && '0' === $value[0]) {
-            // Hex
-            if (\preg_match('/^0x[a-f\d]+$/i', $value)) {
-                return \hexdec($value);
-            }
-            // Octal
-            if (\preg_match('/^0o?[0-7]+$/i', $value)) {
-                if (false !== \strpos('Oo', $value[1])) {
-                    // PHP < 8.1
-                    $value = \substr($value, 2);
-                }
-                return \octdec($value);
-            }
-        }
-        // Exponent
-        if (\preg_match('/^[+-]?\d*[.]?\d+e[+-]?\d+$/i', $value)) {
-            return (float) $value;
-        }
-        if (\is_numeric($value)) {
-            return false !== \strpos($value, '.') ? (float) $value : (int) $value;
-        }
-        // <https://yaml.org/type/timestamp.html>
-        if (\is_numeric($value[0]) && \preg_match('/^[1-9]\d{3,}-(0\d|1[0-2])-(0\d|[1-2]\d|3[0-1])((t|[ \t]+)([0-1]\d|2[0-4]):([0-5]\d|60)(:([0-5]\d|60)([.]\d+)?)?([ \t]*[+-]([0-1]\d|2[0-4]):([0-5]\d|60)(:([0-5]\d|60)([.]\d+)?)?|z)?)?$/i', $value)) {
-            return new \DateTime($value);
-        }
-        if (false === \strpos($value, ":\n") && false === \strpos($value, ":\t") && false === \strpos($value, ': ') && ':' !== \substr($value, -1)) {
-            return from\f($value, false);
-        }
-        $block = -1;
-        $blocks = [];
-        $rows = \explode("\n", $value);
-        foreach ($rows as $row) {
-            $dent = \strspn($row, ' ');
-            $current = $dent > 0 ? \substr($row, $dent) : $row;
-            if ($prev = $blocks[$block] ?? 0) {
-                // A blank line
-                if ("" === $current) {
-                    $blocks[$block] .= "\n";
-                    continue;
-                }
-                if (false !== \strpos($prev, '[') && ']' === $current || false !== \strpos($prev, '{') && '}' === $current) {
-                    $blocks[$block] .= "\n" . $current;
-                    continue;
-                }
-                // A list
-                if ('-' === $current) {
-                    $blocks[$block] .= "\n- ";
-                    continue;
-                }
-                if ('-' === $current[0] && false !== \strpos(" \t", $current[1])) {
-                    $blocks[$block] .= "\n- " . \substr($current, 2);
-                    continue;
-                }
-                if ($dent > 0) {
-                    if ("\n- " === \substr($prev, -3)) {
-                        $blocks[$block] .= $current;
-                        continue;
-                    }
-                    $blocks[$block] .= "\n" . $current;
-                    continue;
-                }
-            }
-            $blocks[++$block] = $current;
-        }
-        $out = [];
-        foreach ($blocks as $block) {
-            if (false !== \strpos('"\'', $block[0]) && \preg_match('/^(' . from\str . '):\s+/', $block, $m)) {
-                $out[from($m[1])] = from(\substr($block, \strlen($m[0])), $array, $lot);
-                continue;
-            }
-            [$k, $s, $v] = \array_replace(["", "", ""], \preg_split('/[ \t]*:([ \n\t]\s*|$)/', $block, 2, \PREG_SPLIT_DELIM_CAPTURE | \PREG_SPLIT_NO_EMPTY));
-            if ("" === $v) {
-                $out[$k] = null;
-                continue;
-            }
-            // Fix case for invalid key-value pair(s) such as `asdf: asdf: asdf` as it should be `asdf:\n asdf: asdf`
-            if ("\n" !== \substr($s, -1) && false === \strpos('!&*:>[{|', $v[0]) && (false !== \strpos($v, ":\n") || false !== \strpos($v, ":\t") || false !== \strpos($v, ': '))) {
-                $out[$k] = $v;
-                continue;
-            }
-            $out[$k] = from($v, $array, $lot);
-        }
-        return $array ? $out : (object) $out;
+        return from\v($value, $array, $lot);
     }
 }
 
@@ -340,5 +139,205 @@ namespace x\y_a_m_l\from {
             }
         }
         return $value;
+    }
+    function v(?string $value, $array = false, array &$lot = []) {
+        if ("" === ($value = \trim($raw = $value ?? ""))) {
+            return null;
+        }
+        // Normalize line break(s)
+        $value = \strtr($value, [
+            "\r\n" => "\n",
+            "\r" => "\n"
+        ]);
+        if ("" === ($value = \trim(c($value)))) {
+            return null;
+        }
+        if (\array_key_exists($var = \strtolower($value), $vars = [
+            "''" => "",
+            '""' => "",
+            '+.inf' => \INF,
+            '+.nan' => \NAN,
+            '-.inf' => -\INF,
+            '-.nan' => -\NAN,
+            '.inf' => \INF,
+            '.nan' => \NAN,
+            '[]' => [],
+            'false' => false,
+            'null' => null,
+            'true' => true,
+            '{}' => $array ? [] : (object) [],
+            '~' => null
+        ])) {
+            return $vars[$var];
+        }
+        if ('"' === $value[0] && '"' === \substr($value, -1)) {
+            return \stripcslashes(f(\strtr(\substr($value, 1, -1), [
+                "\\\n" => ""
+            ])));
+        }
+        if ("'" === $value[0] && "'" === \substr($value, -1)) {
+            return f(\strtr(\substr($value, 1, -1), [
+                "''" => "'"
+            ]), false);
+        }
+        // Fold-style or literal-style value
+        if (false !== \strpos('>|', $value[0])) {
+            [$rule, $content] = \array_replace(["", ""], \explode("\n", c(\ltrim($raw)), 2));
+            $dent = \strspn(\trim($content, "\n"), ' ');
+            $content = \substr(\strtr("\n" . $content, [
+                "\n" . \str_repeat(' ', $dent) => "\n"
+            ]), 1);
+            if (isset($rule[1])) {
+                $cut = \substr($rule, -1);
+                // `>4`
+                if (\is_numeric($cut)) {
+                    $cut = "";
+                    $dent = (int) \substr($rule, 1);
+                // `>4+`
+                } else {
+                    $dent = (int) \substr($rule, 1, -1);
+                }
+            // `>`
+            } else {
+                $cut = "";
+                $dent = 0;
+            }
+            if ("" !== $cut && false === \strpos('+-', $cut)) {
+                return null; // :(
+            }
+            if ('+' !== $cut) {
+                $content = \rtrim($content) . ("" === $cut ? "\n" : "");
+            }
+            if ('>' === $rule[0]) {
+                $content = f($content);
+            }
+            if ($dent > 0) {
+                $d = \str_repeat(' ', $dent);
+                $content = \substr(\strtr(\strtr("\n" . $content, [
+                    "\n" => "\n" . $d
+                ]), [
+                    "\n" . $d . "\n" => "\n\n"
+                ]), 1);
+            }
+            return $content;
+        }
+        if ('[' === $value[0] && ']' === \substr($value, -1) || '{' === $value[0] && '}' === \substr($value, -1)) {
+            return v(r($value), $array, $lot);
+        }
+        // A tag
+        if ('!' === $value[0]) {
+            [$tag, $content] = \array_replace(["", ""], \preg_split('/\s+/', $value, 2, \PREG_SPLIT_NO_EMPTY));
+            $value = v($content, $array, $lot);
+            if ('!!str' === $tag && !isset($lot[$tag]) && $value instanceof \DateTime) {
+                return $content;
+            }
+            return t($value, $array, $lot, $tag);
+        }
+        // <https://yaml.org/spec/1.2.2#692-node-anchors>
+        if (false !== \strpos('&*', $value[0]) && \preg_match('/^([&*])([^\s,\[\]{}]+)(\s+|$)/', $value, $m)) {
+            $key = '&' . $m[2];
+            if ('&' === $m[1]) {
+                return ($lot[$key] = v(\substr($value, \strlen($m[0])), $array, $lot));
+            }
+            return $lot[$key] ?? null;
+        }
+        // List-style value
+        if ('-' === $value[0] && \strlen($value) > 2 && false !== \strpos(" \n\t", $value[1])) {
+            $out = [];
+            foreach (\preg_split('/\n-[ \n\t]/', \substr($value, 2)) as $v) {
+                if (0 === \strpos($v, '- ')) {
+                    $v = \strtr($v, [
+                        "\n  " => "\n"
+                    ]);
+                } else {
+                    $v = \trim($v);
+                }
+                $out[] = v($v, $array, $lot);
+            }
+            return $out;
+        }
+        if (\strlen($value) > 2 && '0' === $value[0]) {
+            // Hex
+            if (\preg_match('/^0x[a-f\d]+$/i', $value)) {
+                return \hexdec($value);
+            }
+            // Octal
+            if (\preg_match('/^0o?[0-7]+$/i', $value)) {
+                if (false !== \strpos('Oo', $value[1])) {
+                    // PHP < 8.1
+                    $value = \substr($value, 2);
+                }
+                return \octdec($value);
+            }
+        }
+        // Exponent
+        if (\preg_match('/^[+-]?\d*[.]?\d+e[+-]?\d+$/i', $value)) {
+            return (float) $value;
+        }
+        if (\is_numeric($value)) {
+            return false !== \strpos($value, '.') ? (float) $value : (int) $value;
+        }
+        // <https://yaml.org/type/timestamp.html>
+        if (\is_numeric($value[0]) && \preg_match('/^[1-9]\d{3,}-(0\d|1[0-2])-(0\d|[1-2]\d|3[0-1])((t|[ \t]+)([0-1]\d|2[0-4]):([0-5]\d|60)(:([0-5]\d|60)([.]\d+)?)?([ \t]*[+-]([0-1]\d|2[0-4]):([0-5]\d|60)(:([0-5]\d|60)([.]\d+)?)?|z)?)?$/i', $value)) {
+            return new \DateTime($value);
+        }
+        if (false === \strpos($value, ":\n") && false === \strpos($value, ":\t") && false === \strpos($value, ': ') && ':' !== \substr($value, -1)) {
+            return f($value, false);
+        }
+        $block = -1;
+        $blocks = [];
+        $rows = \explode("\n", $value);
+        foreach ($rows as $row) {
+            $dent = \strspn($row, ' ');
+            $current = $dent > 0 ? \substr($row, $dent) : $row;
+            if ($prev = $blocks[$block] ?? 0) {
+                // A blank line
+                if ("" === $current) {
+                    $blocks[$block] .= "\n";
+                    continue;
+                }
+                if (false !== \strpos($prev, '[') && ']' === $current || false !== \strpos($prev, '{') && '}' === $current) {
+                    $blocks[$block] .= "\n" . $current;
+                    continue;
+                }
+                // A list
+                if ('-' === $current) {
+                    $blocks[$block] .= "\n- ";
+                    continue;
+                }
+                if ('-' === $current[0] && false !== \strpos(" \t", $current[1])) {
+                    $blocks[$block] .= "\n- " . \substr($current, 2);
+                    continue;
+                }
+                if ($dent > 0) {
+                    if ("\n- " === \substr($prev, -3)) {
+                        $blocks[$block] .= $current;
+                        continue;
+                    }
+                    $blocks[$block] .= "\n" . $current;
+                    continue;
+                }
+            }
+            $blocks[++$block] = $current;
+        }
+        $out = [];
+        foreach ($blocks as $block) {
+            if (false !== \strpos('"\'', $block[0]) && \preg_match('/^(' . str . '):\s+/', $block, $m)) {
+                $out[v($m[1])] = v(\substr($block, \strlen($m[0])), $array, $lot);
+                continue;
+            }
+            [$k, $s, $v] = \array_replace(["", "", ""], \preg_split('/[ \t]*:([ \n\t]\s*|$)/', $block, 2, \PREG_SPLIT_DELIM_CAPTURE | \PREG_SPLIT_NO_EMPTY));
+            if ("" === $v) {
+                $out[$k] = null;
+                continue;
+            }
+            // Fix case for invalid key-value pair(s) such as `asdf: asdf: asdf` as it should be `asdf:\n asdf: asdf`
+            if ("\n" !== \substr($s, -1) && false === \strpos('!&*:>[{|', $v[0]) && (false !== \strpos($v, ":\n") || false !== \strpos($v, ":\t") || false !== \strpos($v, ': '))) {
+                $out[$k] = $v;
+                continue;
+            }
+            $out[$k] = v($v, $array, $lot);
+        }
+        return $array ? $out : (object) $out;
     }
 }
