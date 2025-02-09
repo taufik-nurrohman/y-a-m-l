53a06d6df11488ca1f5995b637dec80550edfacc
Taufik Nurrohman <hi@taufik-nurrohman.com>
2025-02-09 20:49:20 +0700

    TODO: Test for Balance Pair(s)

---
+++ test/from/array/4.2.php
@@ -0,0 +1,29 @@
+<?php
+
+return (object) array(
+  'asdf-1' => array(
+    (object) array(
+      'asdf-1' => array(
+        (object) array(
+          'asdf-1' => array(
+            (object) array(
+              'asdf-1' => array(
+                'asdf',
+                'asdf'
+              )
+            ),
+            (object) array(
+              'asdf-2' => 'asdf'
+            ),
+            (object) array(
+              'asdf-3' => 'asdf'
+            )
+          )
+        )
+      )
+    ),
+    (object) array(
+      'asdf-2' => 'asdf'
+    )
+  )
+);
