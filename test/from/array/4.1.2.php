efe8e671fdf7ddaea84472fd52424ce1fe5401fd
Taufik Nurrohman <hi@taufik-nurrohman.com>
2025-02-13 18:20:45 +0700

    Update

---
+++ test/from/array/4.1.2.php
@@ -0,0 +1,30 @@
+<?php
+
+return (object) array(
+  'asdf-1' => array(
+    (object) array(
+      'asdf-1-1' => array(
+        (object) array(
+          'asdf-1-1-1' => array(
+            (object) array(
+              'asdf-1-1-1-1' => array(
+                'asdf',
+                'asdf'
+              )
+            ),
+            (object) array(
+              'asdf-1-1-1-2' => 'asdf'
+            ),
+            (object) array(
+              'asdf-1-1-1-3' => 'asdf'
+            )
+          )
+        )
+      )
+    ),
+    (object) array(
+      'asdf-1-2' => 'asdf'
+    )
+  ),
+  'asdf-2' => 'asdf'
+);
